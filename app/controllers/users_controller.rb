d522c2249802bd1476f1083ffe5923ab2885afb2
Busyra Anwar <busyra@me.com>
2015-06-04 00:00:09 -0700

    sessions controller created and log in page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,12 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to users_path
+			redirect_to root_path, notice: "Successfully signed up!"
 		else
 			render :new
 		end
 	end
+
 
 	private 
 	def user_params
